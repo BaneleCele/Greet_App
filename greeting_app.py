8a4ae462e49d9ba2dfa9e321366703d29bae0b3f
BaneleCele <banelecele7@gmail.com>
2025-03-21 21:12:24 +0200

    Initial commit

---
+++ greeting_app.py
@@ -0,0 +1,9 @@
+#Create a program that asks for the user’s name and favorite color, then prints a personalized greeting like: “Hello, [Name]! Your favorite color, [Color], is awesome!”
+
+# Prompt the user for their name
+name = input("What is your name? ")
+# Prompt the user for their favorite colour
+colour = input("What is your favorite colour? ")
+    
+# Print a personalized greeting
+print(f"Hello, {name}! Your favorite colour, {colour}, is awesome!")
